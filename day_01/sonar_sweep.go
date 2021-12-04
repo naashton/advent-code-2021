9de9aadf64c37d37b6b7d6bcc2734664c62cd892
naashton <nashton@gmail.com>
2021-12-04 11:35:53 -0500

    Day 1, part 2 solution and refactor

--- day_01/sonar_sweep.go
+++ day_01/sonar_sweep.go
@@ -13,27 +13,59 @@ func check(e error) {
 	}
 }
 
+func count_depth_increments(depths []int) {
+	var prev int
+	var count int
+
+	for _, depth := range depths {
+		if prev == 0 {
+			prev = depth
+		} else if depth > prev {
+			count += 1
+			prev = depth
+		} else {
+			prev = depth
+		}
+	}
+
+	fmt.Println("Number of depth increments is: ", count)
+}
+
+func count_summed_depth_increments(depths []int, window_size int) {
+	var count int
+
+	for idx, _ := range depths {
+		if sum_arr(depths[idx+1:idx+window_size+1]) > sum_arr(depths[idx:idx+window_size]) {
+			count += 1
+		}
+	}
+	fmt.Println("Number of depth increments for window size {", window_size, "} is: ", count)
+}
+
+func sum_arr(depths []int) int{
+	depth_total := 0
+	for _, depth := range depths {
+		depth_total += depth
+	}
+	return depth_total
+}
+
 func main() {
 	file, err := os.Open("input.txt")
 	check(err)
 
 	scanner := bufio.NewScanner(file)
-	var prev int
-	var count int
+	var depths []int
 
 	for scanner.Scan() {
 		val, err := strconv.Atoi(scanner.Text())
 		if err != nil {
 			panic(err)
 		}
-		if prev == 0 {
-			prev = val
-		} else if val > prev {
-			count += 1
-			prev = val
-		} else {
-			prev = val
-		}
+
+		depths = append(depths, val)
 	}
-	fmt.Println(count)
+
+	count_depth_increments(depths)
+	count_summed_depth_increments(depths, 3)
 }
